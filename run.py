aaa3620aa4a2cc5c21b1398ef4394c8996120ce8
aqjune <aqjune@gmail.com>
2018-12-01 16:57:20 +0900

    Add build

--- run.py
+++ run.py
@@ -18,6 +18,19 @@ def checkLLVMConfigForClone(js):
   for i in js["repo"]:
     assert ("url" in js["repo"][i])
 
+def checkLLVMConfigForBuild(js, buildopt):
+  # Destination of src should exist
+  assert ("src" in js)
+  # Should have build options
+  assert ("builds" in js)
+  found = False
+  for i in js["builds"]:
+    if i["type"] == buildopt:
+      assert ("path" in i)
+      assert ("sharedlib" in i)
+      found = True
+  assert(found)
+
 
 class LLVMScript(object):
 
@@ -89,6 +102,65 @@ Type 'python3 run.py <command> help' to get details
     except OSError as e:
       print ("Cannot create directory '{0}'.".format(cfg["src"]))
       exit(1)
+
+
+  def build(self):
+    parser = argparse.ArgumentParser(
+        description = 'Arguments for build command')
+    parser.add_argument('--cfg', help='config path for LLVM (json file)', action='store',
+        required=True)
+    parser.add_argument('--build', help='release/relassert/debug', action='store', required=True)
+    parser.add_argument('--core', help='# of cores to use', nargs='?', const=1, type=int)
+    args = parser.parse_args(sys.argv[2:])
+
+    cfgpath = args.cfg
+    f = open(cfgpath)
+    cfg = json.load(f)
+    checkLLVMConfigForBuild(cfg, args.build)
+
+    options = None
+    for i in cfg["builds"]:
+      if i["type"] == args.build:
+        options = i
+        break
+
+    assert(options != None)
+    if not os.path.exists(options["path"]):
+      try:
+        os.makedirs(options["path"])
+      except OSError as e:
+        print ("Cannot create directory '{0}'.".format(options["path"]))
+        exit(1)
+
+    os.chdir(options["path"])
+
+    cmd = ["cmake", cfg["src"] + "/llvm"]
+    if args.build == "release":
+      cmd.append("-DCMAKE_BUILD_TYPE=Release")
+    elif args.build == "relassert":
+      cmd = cmd + ["-DCMAKE_BUILD_TYPE=Release", "-DLLVM_ENABLE_ASSERTIONS=On"]
+    elif args.build == "debug":
+      cmd.append("-DCMAKE_BUILD_TYPE=Debug")
+
+    if options["sharedlib"]:
+      cmd.append("-DBUILD_SHARED_LIBS=1")
+
+    if "clang" in cfg["repo"]:
+      cmd.append("-DLLVM_EXTERNAL_CLANG_SOURCE_DIR=" + cfg["src"] + "/clang")
+
+    print(cmd)
+
+    p = Popen(cmd)
+    p.wait()
+
+    # Now build
+    cmd = ["cmake", "--build", "."]
+    if args.core:
+      cmd = cmd + ["--", "-j{}".format(str(args.core))]
+
+    p = Popen(cmd)
+    p.wait()
+
 
 if __name__ == '__main__':
   LLVMScript()
